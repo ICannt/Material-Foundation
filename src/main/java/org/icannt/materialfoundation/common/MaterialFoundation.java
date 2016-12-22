3cda08d169cf3508842127afe2402b78d99e3776
LiamEarle <liam.wilkinson@gmail.com>
2016-12-22 23:17:11 +1000

    Fix localisations... again.

--- src/main/java/org/icannt/materialfoundation/common/MaterialFoundation.java
+++ src/main/java/org/icannt/materialfoundation/common/MaterialFoundation.java
@@ -1,7 +1,6 @@
 package org.icannt.materialfoundation.common;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -15,16 +14,12 @@ import org.icannt.materialfoundation.common.proxy.CommonProxy;
 @Mod(modid = MaterialFoundation.MOD_ID, name = MaterialFoundation.MOD_NAME, version = MaterialFoundation.VERSION)
 public class MaterialFoundation {
 
-    public static final String MOD_ID = "materialfoundation";
-    public static final String MOD_NAME = "Material Foundation 2";
-    public static final String VERSION = "1.0";
-
-    @Instance(MOD_ID)
-    public static MaterialFoundation instance;
-
     @SidedProxy(serverSide = "org.icannt.materialfoundation.common.proxy.ServerProxy", clientSide = "org.icannt.materialfoundation.client.proxy.ClientProxy")
     public static CommonProxy proxy;
 
+    public static final String MOD_ID = "materialfoundation";
+    public static final String MOD_NAME = "Material Foundation 2";
+    public static final String VERSION = "1.0";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
