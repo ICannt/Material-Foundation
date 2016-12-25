6efd89814162845c0f5f4d80358b36aedbfd7e0e
trabpukcip <trab@icannt.org>
2016-12-25 17:38:49 +0800

    Fix concrete variants
    Fix concrete variants

--- src/main/java/org/icannt/materialfoundation/common/block/BlockCompositeConcrete.java
+++ src/main/java/org/icannt/materialfoundation/common/block/BlockCompositeConcrete.java
@@ -1,9 +1,11 @@
 package org.icannt.materialfoundation.common.block;
 
+import java.util.List;
 import java.util.Map;
 
 import org.icannt.materialfoundation.common.MaterialFoundation;
 import org.icannt.materialfoundation.common.block.state.EnumCompositeType;
+import org.icannt.materialfoundation.common.block.state.EnumMetalType;
 import org.icannt.materialfoundation.common.creativetab.TabMaterialFoundation;
 
 import net.minecraft.block.Block;
@@ -15,10 +17,13 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
@@ -43,6 +48,13 @@ public class BlockCompositeConcrete extends Block {
         return new BlockStateContainer(this, COMPOSITE_VARIANT);
     }
 
+    @Override
+    public void getSubBlocks(Item item, CreativeTabs tab, List<ItemStack> list) {
+        for (EnumCompositeType type : EnumCompositeType.values()) {
+            list.add(new ItemStack(this, 1, type.ordinal()));
+        }
+    }
+    
     @SuppressWarnings("deprecation")
     @Override
     public IBlockState getStateFromMeta(int meta) {
@@ -52,6 +64,16 @@ public class BlockCompositeConcrete extends Block {
     @Override
     public int getMetaFromState(IBlockState state) {
         return state.getValue(COMPOSITE_VARIANT).ordinal();
+    }
+    
+    @Override
+    public int damageDropped(IBlockState state) {
+        return getMetaFromState(state);
+    }
+
+    @Override
+    public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return state.getValue(COMPOSITE_VARIANT).getLight();
     }
 
     @SuppressWarnings("deprecation")
