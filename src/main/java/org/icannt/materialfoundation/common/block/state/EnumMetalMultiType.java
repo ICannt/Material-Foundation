c6988c0b6fce0e33d6122c7c01a9d5e2432c908e
trabpukcip <trab@icannt.org>
2016-12-30 20:39:04 +0800

    More crate blocks, studded floor textures, revised studded wall textures
    More crate blocks, studded floor textures, revised studded wall textures

--- src/main/java/org/icannt/materialfoundation/common/block/state/EnumMetalMultiType.java
+++ src/main/java/org/icannt/materialfoundation/common/block/state/EnumMetalMultiType.java
@@ -9,7 +9,12 @@ import net.minecraft.util.IStringSerializable;
 public enum EnumMetalMultiType implements IStringSerializable {
 
 	IRON ("iron", 0, 5, 30, EnumRarity.COMMON),
-	IRON_GOLD ("iron_gold", 0, 5, 30, EnumRarity.COMMON);
+	IRON_GOLD ("iron_gold", 0, 5, 30, EnumRarity.COMMON),
+	TIN_IRON ("tin_iron", 0, 5, 30, EnumRarity.COMMON),
+	DARK_STEEL_BLAZE ("dark_steel_blaze", 0, 5, 30, EnumRarity.COMMON),
+	DARK_STEEL_LAPIS ("dark_steel_lapis", 0, 5, 30, EnumRarity.COMMON),
+	DARK_STEEL_SLIME ("dark_steel_slime", 0, 5, 30, EnumRarity.COMMON),
+	FLUXED_GOLD ("fluxed_gold", 0, 5, 30, EnumRarity.COMMON);
 
     private String name;
     private int light;
